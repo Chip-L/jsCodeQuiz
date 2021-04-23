74c14596ff4eb7157752805c75469c71bf22d27d
Chip Long <chip_long@yahoo.com>
2021-04-23 09:45:55 -0600

    should have initial display good now. Added quizpanel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 // This is the opening display panel
 let openDisp = document.querySelector(".openingPanel");
 let startBtn = document.querySelector(".startButton");
+let main = document.querySelector("main");
+let header = document.querySelector("header");
+let quizDisp = document.querySelector(".quizPanel");
+let questionDisp = document.querySelector(".question");
+let answersDisp = document.querySelector("answers");
 
 startBtn.addEventListener("click", function () {
-  openDisp.setAttribute("style", "display: none;");
+  openDisp.setAttribute("style", "display: none");
+  main.setAttribute("style", "margin-top: none");
+  header.setAttribute("style", "display: flex");
+  quizDisp.setAttribute("style", "display: block");
 });
