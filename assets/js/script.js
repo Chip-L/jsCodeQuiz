ea294b6e60384ea2783d8ca0e6f67d128db79ee6
Chip Long <chip_long@yahoo.com>
2021-04-24 21:29:19 -0600

    quizPanel is displaying ok now. might need more formatting. I have timer done and can display a question.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,59 @@ let startBtn = document.querySelector(".startButton");
 let main = document.querySelector("main");
 let header = document.querySelector("header");
 let quizDisp = document.querySelector(".quizPanel");
-let questionDisp = document.querySelector(".question");
 let answersDisp = document.querySelector("answers");
 
-startBtn.addEventListener("click", function () {
+// timer variables - need accessed from multiple functions
+let timer;
+let timeLeft = 50;
+
+startBtn.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  // local variables
+  let questionNum = 0;
+
   openDisp.setAttribute("style", "display: none");
   main.setAttribute("style", "margin-top: none");
   header.setAttribute("style", "display: flex");
   quizDisp.setAttribute("style", "display: block");
-});
+
+  //enter questions to site
+
+  // pick questions
+
+  displayQuestion(
+    questionNum,
+    "This is the question's text\nand this is the second line."
+  );
+
+  countdown();
+}
+
+// from class work 04.10-Stu_Timers-Intervals
+function countdown() {
+  let timerEl = document.querySelector(".timer");
+
+  // call a function to be executed every 1000 milliseconds
+  timer = setInterval(function () {
+    timeLeft--;
+    if (timeLeft !== 1) {
+      timerEl.textContent = timeLeft + " seconds remaing.";
+    } else {
+      timerEl.textContent = timeLeft + " second remaing.";
+    }
+
+    if (timeLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timer);
+      // Calls function to display the end of game
+      displayMessage();
+    }
+  }, 1000);
+}
+
+function displayQuestion(qNum, qText) {
+  // add 1 to qNum because it is an index, so it starts count at 0
+  document.querySelector("#questionNum").textContent = qNum + 1 + ".";
+  document.querySelector("#questionText").textContent = qText;
+}
